820ca5d2b6ff17e6bcf2ab3f884f0565723c504d
Julia <julmur1@st.amu.edu.pl>
2021-06-02 14:59:38 +0200

    The beggining of the main menu (quitting the game)

--- game_code.py
+++ game_code.py
@@ -5,12 +5,21 @@ from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUI
 # import tkinter to create menu buttons
 from tkinter import *
 
+#initiating pygame
 pygame.init()
+
+# choosing the style of the font for texts
+pygame.font.init()
+font = pygame.font.SysFont("cambria", 20)
+
+
 
 class Background:
     def __init__(self):
+        # define constansts for the screen width and hight
         self.screen_width = 500
         self.screen_height = 500
+        # initiating the screen
         self.screen = pygame.display.set_mode((self.screen_width,
         self.screen_height))
 
@@ -26,7 +35,7 @@ class Background:
         pygame.Rect(position_1, position_2, width, height))
         pygame.display.flip()
 
-# Main loop
+
 kibo_bg = Background()
 ground = Background()
 platform_1 = Background()
@@ -34,9 +43,35 @@ platform_2 = Background()
 platform_3 = Background()
 platform_4 = Background()
 
+
+# define menu method with buttons
+def main_menu():
+
+    # variable to keep the main loop running
+    running = True
+    while running:
+        
+        # look at every event in the queue
+        for event in pygame.event.get():
+
+            # did the user click the window close button? If so, stop the loop.
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            # did the user hit a key?
+            if event.type == KEYDOWN:
+                # was it the escape key? If so, stop the loop.
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+
+
+
+
 # Main loop:
 run = True
-
 while run:
     ground.platforms(0, 470, 500, 30)
     platform_1.platforms(78, 370, 64, 10)
@@ -44,8 +79,5 @@ while run:
     platform_3.platforms(289, 240, 62, 10)
     platform_4.platforms(370, 200, 120, 15)
     kibo_bg.bg_image()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        pygame.display.update()
+
 pygame.quit()
